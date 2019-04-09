ad049b3b4b871f86b4cb7d7b372b743467879639
Vishl Shah <vishu099@gmail.com>
2019-04-09 19:56:33 +0530

    set user and role permission section

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::resource('roles','RolesController');
+    Route::resource('users','UsersController');
+    Route::resource('teams','TeamController');
 });
